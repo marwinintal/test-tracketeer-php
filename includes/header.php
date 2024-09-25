d1c83920540fcde8ce451468d762db8fd295a8a9
Marwin Intal <marwinintal@gmail.com>
2024-09-25 08:59:21 +0800

    Added header and footer

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'classes/RegistrationHandler.php';
+
+$registrationHandler = new RegistrationHandler();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tracketeer Development Exam</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- Styles -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
